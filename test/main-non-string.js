bf6ac21acd87b5b24c291bf9bf5ec84312f1d58f
Gar <gar+gh@danger.computer>
2022-03-08 10:48:44 -0800

    fix: don't throw exception on invalid `main` attr (#109)
    Callback with an error instead

---
+++ test/main-non-string.js
@@ -0,0 +1,14 @@
+var tap = require('tap')
+var readJson = require('../')
+var path = require('path')
+var p = path.resolve(__dirname, 'fixtures/badmainnonstring.json')
+
+tap.test('non-string main entries', function (t) {
+  var logmsgs = []
+  const warn = (...msg) => logmsgs.push(msg)
+  readJson(p, warn, function (er, data) {
+    t.comment(logmsgs.map(msg => 'Warning: ' + msg.join(' ')).join('\n'))
+    t.match(er, new TypeError('The "main" attribute must be of type string.'))
+    t.end()
+  })
+})
