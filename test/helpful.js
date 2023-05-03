0d4d6b6a6334b11058c33f22a07714014950e16c
Gar <gar+gh@danger.computer>
2023-05-03 12:15:51 -0700

    chore: fix tests for node v20 (#175)
    * chore: fix tests for node v20
    
    node v20 gives a totally different error message parsing the invalid JSON
    
    * chore: raise the floor on coverage

--- test/helpful.js
+++ test/helpful.js
@@ -6,7 +6,7 @@ var p = path.resolve(__dirname, 'fixtures/erroneous.json')
 tap.test('erroneous package data', function (t) {
   readJson(p, function (er, data) {
     t.ok(er instanceof Error)
-    t.ok(er.message.match(/Unexpected token "'" \(0x27\)/))
+    t.equal(er.code, 'EJSONPARSE')
     t.end()
   })
 })
